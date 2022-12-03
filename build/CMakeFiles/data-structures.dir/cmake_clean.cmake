23d85599d0a936a9c93cd830224630822a7c73b2
afsaneh <afsaneh1994@gmail.com>
2022-12-03 14:36:59 -0500

    Implemented clear and pop_back function

---
+++ build/CMakeFiles/data-structures.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data-structures.dir/tests/vector_test.cpp.o"
+  "CMakeFiles/data-structures.dir/tests/vector_test.cpp.o.d"
+  "data-structures"
+  "data-structures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data-structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
